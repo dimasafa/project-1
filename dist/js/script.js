6b36e796eed9de53d43d19cdcf003011d5248edd
Dmitrii Afanasev <dimasafanasyev@gmial.com>
2023-01-18 16:35:36 +0100

    cards aus dataBase

--- dist/js/script.js
+++ dist/js/script.js
@@ -264,6 +264,52 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+
+    // class
+
+    class HappyClient {
+        constructor(src, alt, descr, name, stelle, parentSelctor, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.descr = descr;
+            this.name = name;
+            this.stelle = stelle;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelctor);
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            element.innerHTML = `
+            <div class="client__box">
+                <img src=${this.src} alt=${this.alt} class="client__img">
+                <div class="client__text">
+                    <div class="client__descr">${this.descr}</div>
+                    <div class="client__name">
+                        <div class="client__name-1">${this.name}</div>
+                        <div class="client__name-2">${this.stelle}</div>
+                    </div>
+                </div>
+            </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    async function getResource(url) {
+        let res = await fetch(url);
+    
+        return await res.json();
+    }
+    
+    getResource('http://localhost:3000/clients')
+    .then(data => {
+        data.forEach(({src, alt, descr, name, stelle}) => {
+            new HappyClient(src, alt, descr, name, stelle, ".client__wrapper").render();
+        });
+    });
+
+
+
 });
-
-
